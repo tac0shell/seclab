2441fc23d74516947f8a18bdd142e92b95fe65a5
Patrick Riggs <patrickriggs@gmail.com>
2024-01-01 17:15:20 -0700

    Replacing abandoned project Telmate with TheGameProfi

--- Terraform/ad-lab/main.tf
+++ Terraform/ad-lab/main.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.14"
+      source  = "TheGameProfi/proxmox"
     }
     vault = {
       source  = "hashicorp/vault"
